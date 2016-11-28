6037bba249437e2f956080a375d6d3eb333cd856
Paul Brabban <paul.brabban@gmail.com>
2016-11-28 22:27:40 +0000

    setup a grammar for parsing condition expressions

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :target-path "target/%s"
-  :profiles {})
+  :profiles {:dev {:dependencies [[instaparse "1.4.3"]]}})
---
+++ test/dynamodb_expression/grammar.clj
@@ -0,0 +1,54 @@
+(ns dynamodb-expression.grammar
+  (:require [instaparse.core :as insta]
+            [clojure.test :refer :all]
+            [dynamodb-expression.core :as dx]))
+
+(def parse
+  (insta/parser
+   "S = update-expression (whitespace update-expression)*
+    update-expression = ADD | SET | REMOVE | DELETE
+    ADD = 'ADD' add-clause* last-add-clause
+    last-add-clause = whitespace path whitespace value
+    add-clause = last-add-clause comma
+
+    SET = 'SET' set-clause* last-set-clause
+    last-set-clause = whitespace path whitespace '=' whitespace (set-equal | set-modified)
+    set-equal = value-placeholder
+    set-modified = name-placeholder whitespace operator whitespace value-placeholder
+    set-clause = last-set-clause comma
+
+    REMOVE = 'REMOVE' remove-clause* last-remove-clause
+    last-remove-clause = whitespace path
+    remove-clause = last-remove-clause comma
+
+    DELETE = 'DELETE' delete-clause* last-delete-clause
+    last-delete-clause = whitespace path whitespace value-placeholder
+    delete-clause = last-delete-clause comma
+
+    operator = '+' | '-'
+    path = name-placeholder | path-parts
+    path-parts = path-part+ last-path-part
+    last-path-part = name-placeholder
+    path-part = name-placeholder dot
+    name-placeholder = '#' legal-name | legal-name | list-item
+    list-item = legal-name '[' #'[0-9]+' ']'
+    legal-name = #'[a-zA-Z][a-zA-Z0-9_]*'
+    value = value-placeholder
+    value-placeholder = ':' legal-name
+    whitespace = ' '
+    comma = ','
+    dot = '.'"))
+
+
+(deftest dynamodb-expression-grammar-test
+  (testing "AWS documentation examples parse"
+    (is (vector? (parse "SET list[0] = :val1")))
+    (is (vector? (parse "REMOVE #m.nestedField1, #m.nestedField2")))
+    (is (vector? (parse "ADD aNumber :val2, anotherNumber :val3")))
+    (is (vector? (parse "DELETE aSet :val4")))
+    (is (vector? (parse "SET list[0] = :val1 REMOVE #m.nestedField1, #m.nestedField2 ADD aNumber :val2, anotherNumber :val3 DELETE aSet :val4")))
+    (is (vector? (parse "SET Price = Price - :p")))
+    (is (vector? (parse "REMOVE MyNumbers[1], MyNumbers[3]"))))
+
+  (testing "Badly-formed examples don't parse"
+    (is (map? (parse "foo")))))
