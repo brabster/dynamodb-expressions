58e6c64a31a8f0cd227555048ac133b46eaae49c
Paul Brabban <paul.brabban@gmail.com>
2016-12-04 10:22:18 +0000

    add qa checks, start tidyup

--- project.clj
+++ project.clj
@@ -11,6 +11,27 @@
                               :password :env/clojars_password
                               :sign-releases false}]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
+  :aliases {"qa" ["do"
+                  ["clean"]
+                  ["check"]
+                  ["eastwood"]
+                  ["bikeshed" "-m" "100"]
+                  ["ancient"]
+                  ["cloverage"]]}
+  :eastwood {:include-linters [:keyword-typos
+                               :non-clojure-file
+                               :unused-fn-args
+                               :unused-locals
+                               :unused-namespaces
+                               :unused-private-vars
+                               :unused-private-vars]
+             :exclude-linters [:suspicious-expression]}
+  :plugins [[lein-ancient "0.6.10"]
+            [lein-kibit "0.1.2" :exclusions [org.clojure/clojure
+                                             org.clojure/tools.cli]]
+            [jonase/eastwood "0.2.3"]
+            [lein-bikeshed "0.3.0"]
+            [lein-cloverage "1.0.6"]]
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[instaparse "1.4.3"]
                                   [docker-fixture "0.1.1"]
